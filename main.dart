145e134eb36ce77857864b64ce64ab289b47afaf
Belal Akram Mohamed Alhajj <141582400+belalakram@users.noreply.github.com>
2024-09-04 16:32:40 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,24 @@
+void main() {
+  Car myCar = Car('Blue');
+  print('Car color: ${myCar.getColor()}');
+
+  myCar.setColor('Red');
+  print('New car color: ${myCar.getColor()}');
+}
+
+class Car {
+  String? _color; // Nullable private variable
+
+  Car(String color) {
+    _color = color;
+  }
+
+  String getColor() {
+    return _color ?? 'Unknown'; // Return 'Unknown' if _color is null
+  }
+
+  void setColor(String newColor) {
+    _color = newColor;
+  }
+}
+
