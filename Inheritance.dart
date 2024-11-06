b31f831615a91a72452f8962f3d5f5ccb7b5096d
Belal Akram Mohamed Alhajj <141582400+belalakram@users.noreply.github.com>
2024-11-06 02:37:06 +0200

    Update Inheritance.dart

--- Inheritance.dart
+++ Inheritance.dart
@@ -1,18 +1,63 @@
+// Superclass
+class Vehicle {
+  String color;
+
+  Vehicle(this.color);
+
+  void displayInfo() {
+    print('Vehicle color: $color');
+  }
+}
+
+// Subclass
+class Car extends Vehicle {
+  int numberOfDoors;
+
+  Car(String color, this.numberOfDoors) : super(color);
+
+  void displayCarInfo() {
+    displayInfo(); // Calling method from the superclass
+    print('Number of doors: $numberOfDoors');
+  }
+}
+
 void main() {
-  Dog myDog = Dog();
-  myDog.speak(); // Inherited from Animal
-  myDog.bark();  // Dog-specific behavior
+  Car myCar = Car('Red', 4);
+  myCar.displayCarInfo();
 }
 
-class Animal {
-  void speak() {
-    print('Animal speaks');
+
+//Example of Composition:
+//This allows for greater flexibility and reusability without the constraints of inheritance.
+ // Class 1 + Class 2 <== chiled
+
+// A class representing an Engine
+class Engine {
+  String type;
+
+  Engine(this.type);
+
+  void start() {
+    print('Engine of type $type started.');
   }
 }
 
-class Dog extends Animal {
-  void bark() {
-    print('Dog barks');
+// A class representing a Car that uses composition
+class Car {
+  String color;
+  Engine engine; // Composition: Car has an Engine
+
+  Car(this.color, this.engine);
+
+  void displayInfo() {
+    print('Car color: $color');
+    engine.start(); // Using the engine's method
   }
+}
+
+void main() {
+  Engine myEngine = Engine('V8');
+  Car myCar = Car('Blue', myEngine);
+  myCar.displayInfo();
 }
 
